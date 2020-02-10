fd087c8baecf250a900ee4a372ae4625e569a632
Kérian <kerianmontes@gmail.com>
2020-02-10 13:34:33 +0100

    components use props

--- src/Components/PersonItem.js
+++ src/Components/PersonItem.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Avatar from './Avatar';
-import avatar from "../datas/avatar";
-import name from "../datas/name";
 
 class PersonItem extends React.Component {
     render() {
         return (
             <div>
                 <Avatar
-                    avatar={avatar}
-                    name={name} />
+                    avatar={this.props.avatar}
+                    name={this.props.name} />
 
                 <h4>{this.props.name}</h4>
             </div>
